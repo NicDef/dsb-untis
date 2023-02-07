6f6d945608d06ba8400ac97bca875fb4283699c5
Nicolas Defée <87803877+NicDef@users.noreply.github.com>
2023-02-07 14:52:00 +0100

    Add date and info message

--- src/timetableHtmlParser.js
+++ src/timetableHtmlParser.js
@@ -6,8 +6,32 @@ function parse(html) {
   const dom = new JSDOM(html);
   const $ = jquery(dom.window);
 
-  var items = $("table.mon_list tr");
   const table = [];
+
+  var date = $("div.mon_title").html();
+  table.push(date);
+
+  var info = $("table.info tr");
+  for (var i = 0; i < info.length; i++) {
+    const result = [];
+    const cells = [
+      ...$($(info)[i]).children("th"),
+      ...$($(info)[i]).children("td"),
+    ];
+    for (var j = 0; j < cells.length; j++) {
+      const rawCell = $(cells[j]).html();
+      const strikeCell = $(cells[j]).find("strike").html();
+      const spanCell = $(cells[j]).find("span").html();
+      let cell = spanCell || strikeCell || rawCell;
+      cell = "&nbsp;" == cell ? "" : cell;
+      result.push(cell);
+    }
+    console.debug(i + " #size=" + j + " -> ", result);
+    table.push(result);
+  }
+  console.debug(i + " -> ", table);
+
+  var items = $("table.mon_list tr");
   for (var i = 0; i < items.length; i++) {
     const result = [];
     const cells = [
